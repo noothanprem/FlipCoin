0f46843c87b60481d1fd64e0bc8848e29d38ab82
noothanprem <noothanprem@gmail.com>
2019-11-21 17:59:41 +0530

    add : created the same for triplet combination

--- flipcoin.py
+++ flipcoin.py
@@ -3,7 +3,7 @@ class FlipCoin:
 	
 	singlet_dict = {'H':0,'T':0}
 	doublet_dict = {'HH':0,'HT':0,'TH':0,'TT':0}
-
+	triplet_dict = {'HHH':0,'HHT':0,'HTH':0,'THH':0,'TTT':0,'TTH':0,'THT':0,'HTT':0}
 	def singlet(self,number):
 
 		for flip_count in range(number):
@@ -48,7 +48,54 @@ class FlipCoin:
 		print("TH Percentage : ",th_percentage)
 		print("HT Percentage : ",ht_percentage)
 		print("HH Percentage : ",hh_percentage)
+	
+	def triplet(self,number):
+		
+		for flip_count in range(number):
 
+			first_toss = random.randint(0,1)
+			second_toss = random.randint(0,1)
+			third_toss = random.randint(0,1)
+
+			if first_toss == 0 and second_toss == 0 and third_toss == 0:
+				print("TTT")
+				self.triplet_dict['TTT'] += 1
+			elif first_toss == 0 and second_toss == 0 and third_toss == 1:
+				print("TTH")
+				self.triplet_dict['TTH'] += 1
+			elif first_toss == 0 and second_toss == 1 and third_toss == 0:
+				print("THT")
+				self.triplet_dict['THT'] += 1
+			elif first_toss == 1 and second_toss == 0 and third_toss == 0:
+				print("HTT")
+				self.triplet_dict['HTT'] += 1
+			elif first_toss == 1 and second_toss == 1 and third_toss == 1:
+				print("HHH")
+				self.triplet_dict['HHH'] += 1
+			elif first_toss == 1 and second_toss == 1 and third_toss == 0:
+				print("HHT")
+				self.triplet_dict['HHT'] += 1
+			elif first_toss == 1 and second_toss == 0 and third_toss == 1:
+				print("HTH")
+				self.triplet_dict['HTH'] += 1
+			else:
+				print("THH")
+				self.triplet_dict['THH'] += 1
+
+		print(self.triplet_dict)
+		
+		triplet_percentage_dict = {'HHH':0,'HHT':0,'HTH':0,'THH':0,'TTT':0,'TTH':0,'THT':0,'HTT':0}
+		
+		triplet_percentage_dict['HHH'] = ((self.triplet_dict['HHH'] / number) * 100)
+		triplet_percentage_dict['HHT'] = ((self.triplet_dict['HHT'] / number) * 100)
+		triplet_percentage_dict['HTH'] = ((self.triplet_dict['HTH'] / number) * 100)
+		triplet_percentage_dict['THH'] = ((self.triplet_dict['THH'] / number) * 100)
+		triplet_percentage_dict['TTT'] = ((self.triplet_dict['TTT'] / number) * 100)
+		triplet_percentage_dict['TTH'] = ((self.triplet_dict['TTH'] / number) * 100)
+		triplet_percentage_dict['THT'] = ((self.triplet_dict['THT'] / number) * 100)
+		triplet_percentage_dict['HTT'] = ((self.triplet_dict['HTT'] / number) * 100)
+
+		print(triplet_percentage_dict)
 
 if __name__ == "__main__":
 
@@ -56,4 +103,5 @@ if __name__ == "__main__":
 	number = int(input("How many times you want to flip the coin ? :"))
 	flipcoin_object.singlet(number)
 	flipcoin_object.doublet(number)
+	flipcoin_object.triplet(number)
 
