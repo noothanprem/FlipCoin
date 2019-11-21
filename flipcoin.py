bc5c48a55e630ae5e08e1e057c6ca349bdce726d
noothanprem <noothanprem@gmail.com>
2019-11-21 16:15:23 +0530

    add : loop through multiple times of flipping a coin and store the singlet combination in a dictionary

--- flipcoin.py
+++ flipcoin.py
@@ -1,16 +1,27 @@
 import random
 class FlipCoin:
 	
-	def flip_coin(self):
+	singlet_dict = {'H':0,'T':0}
 
-		toss=random.randint(0,1)
-		if toss == 0:
-			print("Heads")
-		else:
-			print("Tails")
+	def flip_coin(self,number):
+
+		for flip_count in range(number):
+			toss=random.randint(0,1)
+			if toss == 0:
+				print("Heads")
+				self.singlet_dict['H'] += 1
+			else:
+				print("Tails")
+				self.singlet_dict['T'] += 1
+		
+		heads_percentage = (self.singlet_dict['H'] / number) * 100
+		tails_percentage = (self.singlet_dict['T'] / number) * 100
+		print("Heads Percentage : ",heads_percentage)
+		print("Tails Percentage : ",tails_percentage)
 
 if __name__ == "__main__":
 
 	flipcoin_object = FlipCoin()
-	flipcoin_object.flip_coin()
+	number = int(input("How many times you want to flip the coin ? :"))
+	flipcoin_object.flip_coin(number)
 
